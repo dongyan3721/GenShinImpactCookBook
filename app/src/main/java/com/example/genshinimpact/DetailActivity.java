38b010c1b561983dadc4daee10f140aaf8842135
xz559618 <2377258329@qq.com>
2023-12-04 01:32:47 +0800

    version2

--- app/src/main/java/com/example/genshinimpact/DetailActivity.java
+++ app/src/main/java/com/example/genshinimpact/DetailActivity.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
     private ArrayList<Members> arrayOfMembers = new ArrayList<Members>();
+    private ArrayList<characterData> arrayOfCharacter = new ArrayList<characterData>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +27,41 @@ public class DetailActivity extends AppCompatActivity {
         String database_path = getDatabasePath("gen_shin.db").toString();
         SQLiteDatabase myDatabase = SQLiteDatabase.openDatabase(database_path, null, SQLiteDatabase.ENABLE_WRITE_AHEAD_LOGGING);
         // TODO: 2023/12/2 添加id查找
-        List<detailData> detailDatas = findData.findData(myDatabase);
+        List<detailData> detailDatas = dataAction.findData(myDatabase);
+        characterData characterData = dataAction.findAttribute(myDatabase);
 
         //注册链接Adapter
         MembersAdapter adapter = new MembersAdapter(this, arrayOfMembers);
         ListView listView = (ListView) findViewById(R.id.list_view);
         listView.setAdapter(adapter);
 
+
+        //属性页信息
+        addAttribute(characterData);
+
         //添加listview成员
         addMembers(detailDatas);
+
+        Button button = findViewById(R.id.check);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 切换布局的可见性
+                LinearLayout layout1 = findViewById(R.id.layout1);
+                LinearLayout layout2 = findViewById(R.id.layout2);
+                Button button1 = findViewById(R.id.check);
+                if (layout1.getVisibility() == View.VISIBLE) {
+                    layout1.setVisibility(View.GONE);
+                    layout2.setVisibility(View.VISIBLE);
+                    button1.setText("技能信息");
+                } else {
+                    layout1.setVisibility(View.VISIBLE);
+                    layout2.setVisibility(View.GONE);
+                    button1.setText("属性信息");
+                }
+            }
+        });
+
     }
     public void addMembers(List<detailData> detailDatas)
     {
@@ -39,4 +71,33 @@ public class DetailActivity extends AppCompatActivity {
             arrayOfMembers.add(members);
         }
     }
+    public void addAttribute(characterData characterData)
+    {
+            TextView title = findViewById(R.id.title);
+            TextView country = findViewById(R.id.country);
+            TextView star = findViewById(R.id.star);
+            TextView level = findViewById(R.id.level);
+            TextView attribute = findViewById(R.id.attribute);
+            TextView weapen = findViewById(R.id.weapen);
+            TextView god = findViewById(R.id.god);
+            TextView race = findViewById(R.id.race);
+            TextView food = findViewById(R.id.food);
+            TextView time = findViewById(R.id.time);
+            TextView introuduce = findViewById(R.id.intruduce);
+
+            title.setText(characterData.title+"-"+characterData.fullName);
+            country.setText("地区:"+characterData.country);
+            star.setText(characterData.star+"星");
+            level.setText(characterData.level);
+            attribute.setText("属性:"+characterData.attribute);
+            weapen.setText("武器:"+characterData.weapen);
+            god.setText("命座:"+characterData.god);
+            race.setText("种族:"+characterData.race);
+            food.setText("特殊食材:"+characterData.food);
+            time.setText("上线日期:"+characterData.time);
+            introuduce.setText("\n  简介:"+"\n        "+characterData.introduce);
+
+
+    }
+
 }
---
+++ app/src/main/java/com/example/genshinimpact/dataAction.java
@@ -0,0 +1,78 @@
+package com.example.genshinimpact;
+
+import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class dataAction {
+    @SuppressLint("Range")
+    // TODO: 2023/12/2 添加名字查询
+    public static List findData(SQLiteDatabase db){
+        String query1 = "SELECT * FROM 天赋表 WHERE 天赋持有人 = ?";
+
+    //修改
+        String[] selectionArgs = {"魈"};
+
+        // 执行查询操作
+        Cursor cursor1 = db.rawQuery(query1, selectionArgs); //技能查询
+
+        List<detailData> detailDatas = new ArrayList<>();
+        List<characterData> characterDatas = new ArrayList<>();
+
+        // 遍历结果
+        if (cursor1.moveToFirst()) {
+            do {
+                detailData detailData = new detailData();
+                detailData.talentName = cursor1.getString(cursor1.getColumnIndex("天赋名"));
+                detailData.talentDetail = cursor1.getString(cursor1.getColumnIndex("天赋描述"));
+                detailData.picUrl = cursor1.getString(cursor1.getColumnIndex("天赋图"));
+                detailDatas.add(detailData);
+            } while (cursor1.moveToNext());
+        }
+
+        // 关闭游标和数据库连接
+        cursor1.close();
+
+        return detailDatas;
+    }
+
+    @SuppressLint("Range")
+    public static characterData findAttribute(SQLiteDatabase db){
+        String query2 = "SELECT * FROM 角色基础信息表 WHERE 姓名 = ?";
+        //修改
+        String[] selectionArgs = {"魈"};
+
+        // 执行查询操作
+        Cursor cursor2 = db.rawQuery(query2,selectionArgs); //详细查询
+        characterData characterData = new characterData();
+
+        if (cursor2.moveToFirst()) {
+            do {
+
+                characterData.attribute = cursor2.getString(cursor2.getColumnIndex("属性"));
+                characterData.country = cursor2.getString(cursor2.getColumnIndex("所属国家"));
+                characterData.fullName = cursor2.getString(cursor2.getColumnIndex("全名"));
+                characterData.star = cursor2.getString(cursor2.getColumnIndex("星级"));
+                characterData.god = cursor2.getString(cursor2.getColumnIndex("命座"));
+                characterData.level = cursor2.getString(cursor2.getColumnIndex("常驻/限定"));
+                characterData.weapen = cursor2.getString(cursor2.getColumnIndex("武器类型"));
+                characterData.race = cursor2.getString(cursor2.getColumnIndex("种族"));
+                characterData.food = cursor2.getString(cursor2.getColumnIndex("特殊食材"));
+                characterData.title = cursor2.getString(cursor2.getColumnIndex("称号"));
+                characterData.time = cursor2.getString(cursor2.getColumnIndex("上线日期"));
+                characterData.introduce = cursor2.getString(cursor2.getColumnIndex("简介"));
+                // 进行相应的操作
+            } while (cursor2.moveToNext());
+        }
+
+
+        // 关闭游标和数据库连接
+        cursor2.close();
+
+
+        return characterData;
+    }
+}
