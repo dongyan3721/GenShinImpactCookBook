efb9daf8bf423750847a3008015b7e39556a05d3
51zzZ <jackywoo1024@qq.com>
2023-12-18 19:44:54 +0800

    webview

--- app/src/main/java/com/example/myapplication/config/LoginConfig.java
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication.config;
-
-import com.example.myapplication.entity.User;
-
-public class LoginConfig {
-    public static User loginUser;
-}
--- app/src/main/java/com/example/myapplication/config/TokenConfig.java
+++
@@ -1,6 +0,0 @@
-package com.example.myapplication.config;
-
-// 向服务器发送请求要用到这个
-public class TokenConfig {
-    public static String token = "";
-}
